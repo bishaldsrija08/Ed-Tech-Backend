21a376a92bc660f8046f6c5acff3dd2c8493476a
bishaldsrija08 <bishaldsrijal1@gmail.com>
2025-06-04 20:17:36 +0545

    registered and login implemented

--- src/controller/globals/auth/authController.ts
+++ src/controller/globals/auth/authController.ts
@@ -1,46 +1,131 @@
-//Logical Code goes here
-
 /*
 
-Register --> username, email, password
-processing/checking --> valid email, compulsary data
-db -->tablee--> query--> CRUD
-Login
-Logout
-Forgot password/OTP
-Reset password
+REGISTER/SIGNUP
+incoming data  --> username, email, password 
+processing/checking --> email valid, compulsory data aaaunu paryo 
+db--> table--> query --> table ma insert/read/delete/update
+
+LOGIN/SIGNIN
+LOGOUT
+FORGOT PASSWORD 
+RESET PASSWORD/ OTP
+
 */
 
-import { Request, Response } from 'express'
-import User from '../../../database/models/userModel'
-//form data => req.body // username, email, password
-//files => req.file // files
+import { Request, Response } from "express"
+import User from "../../../database/models/userModel"
+import bcrypt from 'bcrypt'
+
+// json data --> req.body // username,email,password 
+// files --> req.file // files
+// const registerUser = async (req:Request,res:Response)=>{
+// //    const username = req.body.username
+// //    const password = req.body.password
+// //    const email = req.body.email
+//     // incoming data --> accept
+//    const {username,password,email} = req.body
+//    if(!username || !password || !email){
+//      res.status(400).json({
+//         message : "Please provide username, password, email"
+//     })
+//     return
+//    }
+//     // insert into Users table 
+//     await User.create({
+//         username :username, 
+//         password : password, 
+//         email : email
+//     })
+//     res.status(200).json({
+//         message : "User registered successfully"
+//     })
 
 
-const registerUser = async (req: Request, res: Response) => {
-    // const username = req.body.username
-    // const password = req.body.password
-    // const email = req.body.email
+// } // function 
+// BOLA Attack
 
-    //incoming data accept
-    const { username, email, password } = req.body //object desturcturing
-    if (!username || !email || !password) {
-        res.status(400).json({
-            message: "Please provide username, password, email!"
+
+class AuthController {
+    static async registerUser(req: Request, res: Response) {
+        if (req.body == undefined) {
+            res.status(400).json({
+                message: "No data was sent!!"
+            })
+            return
+        }
+        const { username, password, email } = req.body
+        if (!username || !password || !email) {
+            res.status(400).json({
+                message: "Please provide username, password, email"
+            })
+            return
+        }
+        //    const [data] =  await User.findAll({
+        //         where : {
+        //             email
+        //         }
+        //     })
+        //     if(data){
+        //         // already exists with that email 
+        //     }
+        // insert into Users table 
+        await User.create({
+            username: username,
+            password: bcrypt.hashSync(password, 12),
+            email: email
         })
-    }else{
-        //insert into users table
-     await User.create({
-            username,
-            email,
-            password
+        res.status(201).json({
+            message: "User registered successfully"
         })
-
-        res.status(200).json({
-            message: "Registered Successfully!"
-        })
-
     }
 
+    //login user
+    static async loginUser(req: Request, res: Response) {
+        if (req.body == undefined) {
+            res.status(400).json({
+                message: "No data was sent!!"
+            })
+        }
+        return
+
+        const { email, password } = req.body
+        if (!email || !password) {
+            res.status(400).json({
+                message: "Please provide email and password!"
+            })
+        }
+
+        //check email
+        const [data] = await User.findAll({
+            where: {
+                email
+            }
+        })
+
+        if (data) {
+            const isMatched = bcrypt.compareSync(password, data.password)
+
+            if (isMatched) {
+                //generate token
+                res.status(200).json({
+                    message: "Login Successfully!"
+                })
+            } else {
+                res.status(400).json({
+                    message: "Invalid password!"
+                })
+            }
+
+        } else {
+            res.status(400).json({
+                message: "email not registered!"
+            })
+        }
+
+    }
 }
-export {registerUser}
+
+export default AuthController
+
+
+// export  {registerUser}
